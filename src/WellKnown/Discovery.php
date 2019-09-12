75147b0681dc58b0c8526e9ec773e0fa0c51b43f
JerryHopper <hopper.jerry@gmail.com>
2019-09-12 11:15:20 +0200

    Update Discovery.php

--- src/WellKnown/Discovery.php
+++ src/WellKnown/Discovery.php
@@ -19,15 +19,13 @@ class Discovery
         if($useservice!=false){
             $class = __NAMESPACE__.'\\Services\\'.str_replace('-','', str_replace('.','', basename($useservice) ) );
         }
-        echo "Start!\n";
-        echo $class."\n";
         if( !class_exists( $class ) ){
             throw new \Exception("Unsupported service discovery endpoint");
         }
 
         $res = new $class($discoveryurl);
         $this->result = $res->get();
-        echo "end!\n";
+
         /*
             // http://openid.net/specs/openid-connect-discovery-1_0.html
             // http://www.iana.org/go/rfc8414
