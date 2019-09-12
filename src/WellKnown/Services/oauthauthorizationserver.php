7cae8068da73337ac8329eb7169449209dda80e2
JerryHopper <hopper.jerry@gmail.com>
2019-09-12 10:37:39 +0200

    Create oauthauthorizationserver.php

---
+++ src/WellKnown/Services/oauthauthorizationserver.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace WellKnown\Services;
+use WellKnown\DiscoveryService;
+use WellKnown\Discovery;
+
+
+class oauthauthorizationserver extends DiscoveryService
+{
+    var $contentType = 'application/json';
+
+    function parseToArray($string){
+        $array = json_decode($string,true);
+        if(array_key_exists('jwks_uri')){
+            $discovery = new Discovery($array['jwks_uri']);
+            $array['jwks_uri'] = $discovery->result;
+        }
+        return $array;
+    }
+}
