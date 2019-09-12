e1854dfeee75e3f260458c50e925f8240675a74a
jerryhopper <hopper.jerry@gmail.com>
2019-09-12 13:10:06 +0200

    Update Discovery.php

--- src/Discovery.php
+++ src/Discovery.php
@@ -9,12 +9,20 @@ use GuzzleHttp\Exception\RequestException;
 
 class Discovery
 {
+    private $result;
     function __construct($discoveryurl,$useservice=false)
     {
         $this->_start($discoveryurl,$useservice);
 
     }
-
+    public function  __get($name) {
+        // check if the named key exists in our array
+        if(array_key_exists($name, $this->result)) {
+            // then return the value from the array
+            return $this->result[$name];
+        }
+        return null;
+    }
     function _start(String $discoveryurl,$useservice){
 
         $class = __NAMESPACE__.'\\Services\\'.str_replace('-','', str_replace('.','', basename($discoveryurl) ) );
