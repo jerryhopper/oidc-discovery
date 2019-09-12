c52bc8b7c93a35e0da338d3cba387d3138eae1a7
JerryHopper <hopper.jerry@gmail.com>
2019-09-12 11:15:55 +0200

    Update oauthauthorizationserver.php

--- src/WellKnown/Services/oauthauthorizationserver.php
+++ src/WellKnown/Services/oauthauthorizationserver.php
@@ -13,7 +13,7 @@ class oauthauthorizationserver extends DiscoveryService
     function parseToArray($string){
         $array = json_decode($string,true);
         if(array_key_exists('jwks_uri')){
-            $discovery = new Discovery($array['jwks_uri']);
+            $discovery = new Discovery($array['jwks_uri'],'jwks.json');
             $array['jwks_uri'] = $discovery->result;
         }
         return $array;
